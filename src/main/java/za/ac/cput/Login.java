3e569db35609758d6c824a8dcc00e928616cb6e5
martinez safari <219325332@mycput.ac.za>
2021-05-06 22:55:26 +0200

    java swing application user login and registration, button modification

--- src/main/java/za/ac/cput/Login.java
+++ src/main/java/za/ac/cput/Login.java
@@ -7,76 +7,90 @@ package za.ac.cput;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.TitledBorder;
 
 public class Login extends JFrame {
-
+    
 public static void main(String[] args) {
     
     Login frameTabel = new Login();
 }
 
 JButton blogin = new JButton("Login");
+JButton bclear = new JButton("Clear");
 JPanel panel = new JPanel();
 JTextField userText = new JTextField("");
 JPasswordField Password = new JPasswordField("");
 
 Login(){ 
-    super("Login Here");
-    setSize(400,300);
+    super("Login");
+    setSize(460,300);
     setLocation(500,280);
     panel.setLayout (null);
+    panel.setBackground(Color.LIGHT_GRAY);
     
-    JLabel user= new JLabel("UserName");
-    user.setBounds(15, 30, 85, 20);
+    panel.setBorder(BorderFactory.createTitledBorder(
+    BorderFactory.createEtchedBorder(), "Login here", TitledBorder.CENTER,
+    TitledBorder.TOP)); 
+    
+    JLabel user= new JLabel("Email:");
+    user.setBounds(15, 50, 90, 25);
     panel.add(user);
     
-    userText= new JTextField(20);
-    userText.setBounds(100, 30, 165, 20);
+    userText= new JTextField();
+    userText.setBounds(100, 50, 170, 25);   
     panel.add(userText);
     
-    JLabel pass= new JLabel("Password");
-    pass.setBounds(15, 55, 85, 20);
+    JLabel example= new JLabel("Example@gmail.com");
+    example.setBounds(275, 50, 170, 25);
+    example.setForeground(Color.gray);
+    panel.add(example);
+    
+    JLabel pass= new JLabel("Password:");
+    pass.setBounds(15, 95, 90, 25); 
     panel.add(pass);
     
-    Password= new JPasswordField();
-    Password.setBounds(100, 55, 165, 20);
+    Password= new JPasswordField(10);
+    Password.setBounds(100, 95, 170, 25);
     panel.add(Password);
          
-    blogin.setBounds(110,110,80,20);
+    blogin.setBounds(40,185,90,20);
+    bclear.setBounds(210,185,90,20);
 
     panel.add(blogin);
+    panel.add(bclear);
     panel.add(userText);
     panel.add(pass);
 
     getContentPane().add(panel);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
     setVisible(true);
     actionlogin();
 }
 public void actionlogin(){
     
     blogin.addActionListener(new ActionListener() {
-        
         public void actionPerformed(ActionEvent e) {
+            String user= userText.getText();
+            String pass= Password.getText();
             
-            String userName = userText.getText();
-            String password = Password.getText();
-    
-            if(userName.equals("test") && password.equals("12345")) 
-            {      
-                Register newf =new Register();
-                newf.setVisible(true);
-                dispose();
-            } 
-            else 
-            {
-                JOptionPane.showMessageDialog(null,"Wrong Password / Username");
-                userText.setText("");
-                Password.setText("");
-                userText.requestFocus();
+            if(user.equals("") && pass.equals("")){
+               blogin.setEnabled(false);   
             }
-
+            else{
+                blogin.setEnabled(true);
+                dispose();
+                new Register().setVisible(true);
+            }  
         }
-    });
+      });
+    bclear.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+        userText.setText("");
+        Password.setText("");
+        
+    }
+         });
+ }
 }
-}
+
--- src/main/java/za/ac/cput/Register.java
+++ src/main/java/za/ac/cput/Register.java
@@ -4,58 +4,129 @@
  */
 package za.ac.cput;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.border.TitledBorder;
 
 public class Register extends JFrame { 
 
-    public static void main(String[] args) {
-    Register frameTabel = new Register();
+    public static void main(String[] args) 
+    {
+        
+        Register frameTabel = new Register();
     }
-
-    JLabel welcome = new JLabel("Welcome to a New Frame");
+    JButton bback = new JButton("Back");
+    JButton bnext = new JButton("Next");
+    JButton bsubmit = new JButton("Submit");
+    
     JPanel panel = new JPanel();
 
     Register(){
-        
+
         super("Register");
-        setSize(400,300);
-        setLocation(500,280);
+        setSize(500,400);
+        setLocation(300,180);
         panel.setLayout (null);
-        
+        panel.setBackground(Color.LIGHT_GRAY);
+
+        panel.setBorder(BorderFactory.createTitledBorder(
+        BorderFactory.createEtchedBorder(), "Login here", TitledBorder.CENTER,
+        TitledBorder.TOP));
+
         JLabel name= new JLabel("Name");
-        name.setBounds(15, 30, 85, 20);
+        name.setBounds(15, 30, 85, 30);
         panel.add(name);
-        
+
         JTextField nameTxt= new JTextField();
-        nameTxt.setBounds(100, 30, 165, 20);
+        nameTxt.setBounds(150, 30, 175, 30);
         panel.add(nameTxt);
-        
+
         JLabel surname= new JLabel("Surname");
-        surname.setBounds(15, 55, 85, 20);
+        surname.setBounds(15, 75, 85, 30);
         panel.add(surname);
-        
-        JTextField surnameTxt= new JPasswordField();
-        surnameTxt.setBounds(100, 55, 165, 20);
+
+        JTextField surnameTxt= new JTextField();
+        surnameTxt.setBounds(150, 75, 175, 30);
         panel.add(surnameTxt);
-        
-        JRadioButton male=new JRadioButton("");
-        panel.add(male);
-        male.setBounds(150,152,25,25);
-        
-        JRadioButton female=new JRadioButton("");
-        panel.add(female);
-        female.setBounds(260,152,25,25);
-        
+
+        JLabel gender= new JLabel("Gender");
+        gender.setBounds(15, 115, 85, 30);
+        panel.add(gender);
+
+        JRadioButton male = new JRadioButton("male", true);
+        JRadioButton female = new JRadioButton("female");
+
         ButtonGroup group = new ButtonGroup();
         group.add(male);
         group.add(female);
+
+        panel.add(male);
+        panel.add(female);
+
+        male.setBounds(150, 115, 65, 30);    
+        female.setBounds(260, 115, 65, 30);  
+
+        JLabel id= new JLabel("ID Number");
+        id.setBounds(15, 155, 85, 30);
+        panel.add(id);
+
+        JTextField idTxt= new JTextField();
+        idTxt.setBounds(150, 155, 175, 35);
+        panel.add(idTxt);
+
+        JLabel contact= new JLabel("Phone Number");
+        contact.setBounds(15, 205, 85, 30);
+        panel.add(contact);
+
+        JTextField contactTxt= new JTextField();
+        contactTxt.setBounds(150, 205, 175, 30);
+        panel.add(contactTxt);
+
+        JLabel address= new JLabel("Address");
+        address.setBounds(15, 255, 85, 30);
+        panel.add(address);
+
+        JTextField addressTxt= new JTextField();
+        addressTxt.setBounds(150, 255, 175, 30);
+        panel.add(addressTxt);
+        
+        bback.setBounds(15,320,100,20);
+        bnext.setBounds(375,320,100,20);
+        bsubmit.setBounds(200,320,100,20);
+
+        panel.add(bback);
+        panel.add(bnext);
+        panel.add(bsubmit);
 
 
         getContentPane().add(panel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        actionRegister();
     }
 
-}
+    private void actionRegister() {
+        bback.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+              dispose();
+               new Login().setVisible(true);
+        }
+      });
+      bnext.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+              dispose();
+               //new next().setVisible(true);
+        }
+      });
+      bsubmit.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+             JOptionPane.showMessageDialog(null,"Well done",  " MsgBox",JOptionPane.PLAIN_MESSAGE);
+        }
+      });
+    }
+ }      
